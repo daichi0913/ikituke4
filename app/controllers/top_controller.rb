b5000b93abbd0cd7b39eaf2d544b952543af32c7
daichi0913 <tonan0913cr@gmail.com>
2019-10-25 11:50:16 +0900

    ビュー完成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,24 @@
 class TopController < ApplicationController
 
+  before_action :manji, except: :index
+
   def index
   end
+
+  def new
+  end
+
+  def create
+    post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.permit(:name, :text)
+  end
+
+  def manji
+    redirect_to action: :index unless user_signed_in?
+  end
 end
+
